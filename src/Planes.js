1095337a174b720084f927b47c171364d7cb1d1e
alexacademiadecodigo <bruno_bsts@yahoo.com>
2024-11-30 16:32:45 +0000

    ficheiro planes inserido

---
+++ src/Planes.js
@@ -0,0 +1,24 @@
+
+
+let dados = [
+    {
+        "id":1,
+        "nome":'A',
+    },
+]
+function Planes(props){
+
+
+    return(
+        <div>
+            <h1>Planes</h1>
+            {dados.map((item, index) => (
+                <div key={index} className="planes">
+                    <h2>{item.nome}</h2>
+                    <h2>{item.id}</h2>
+                </div>
+            ))}
+        </div>
+    );
+}
+export default Planes
