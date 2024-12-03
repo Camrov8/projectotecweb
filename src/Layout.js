3ed227de2aec4666d64e7b5a5ae694db9829877b
Bruno Bastos <bruno.bastos@ua.pt>
2024-12-03 17:31:39 +0000

    Routes a trabalhar

---
+++ src/Layout.js
@@ -0,0 +1,13 @@
+import { Link, Outlet} from "react-router-dom";
+
+function Layout(props){
+return(
+    <>
+       <nav>
+            <Link to="/Planes">Planes</Link>
+             </nav>
+        <Outlet/>
+        </>
+);
+}
+export default Layout
--- src/Planes.js
+++ src/Planes.js
@@ -4,21 +4,26 @@ let dados = [
     {
         "id":2,
         "nome":'A',
+        "img":<img src="plane.jpg" className="img" alt="Planess"  id="planeimg"/>
     },
 ]
 function Planes(props){
 
 
     return(
-        <div>
+        <div className="Planes" >
+            
             <h1>Planes</h1>
             {dados.map((item, index) => (
-                <div key={index} className="planes">
+                <div key={index} className="Planes">
                     <h2>{item.nome}</h2>
-                    <h2>{item.id}</h2>
-                </div>
-            ))}
+                    <h2 >{item.id}</h2>
+                    <h2 >{item.img}</h2>
+                   </div>
+                ))}
+           
         </div>
+        
     );
 }
 export default Planes;
