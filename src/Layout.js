d9d54836596c94b2e5fb27008a9e589798edd6e3
camrov8 <bruno_bsts@yahoo.com>
2024-12-09 01:35:05 +0000

    Layout.css adicionado

--- src/Layout.js
+++ src/Layout.js
@@ -3,10 +3,12 @@ import { Link, Outlet} from "react-router-dom";
 function Layout(props){
 return(
     <>
+       <body className="layoutBody"> 
        <nav>
-            <Link to="/Planes">Planes   </Link>
+            <Link to="/Planes" id='link'>Planes   </Link>
           </nav>
         <Outlet/>
+        </body>
         </>
 );
 }
